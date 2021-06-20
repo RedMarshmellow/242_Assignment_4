e8facf7a1001142167dd6ab82b5319e756dba8a5
RedMarshmellow <ahmedhaniaminmohamed@gmail.com>
2021-06-20 20:37:55 +0300

    filled in info

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /* Student 1: Adam Mohamed I. K. - 2350775
- * Student 2: Ahmed Balalaika - idnum123
+ * Student 2: Ahmed Mohamed - 2351724
 I read and accept the submission rules and the important section specified in assignment file.
 This is my own work that is done by myself and my team-mate only */
 
