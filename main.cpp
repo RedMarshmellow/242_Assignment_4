aee3a37761286e45be5a1088100e2dc521e3ce67
adam <adamqinawi@gmail.com>
2021-06-20 07:10:41 +0200

    inserting starter comment

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+/* Student 1: Adam Mohamed I. K. - 2350775
+ * Student 2: Ahmed Balalaika - idnum123
+I read and accept the submission rules and the important section specified in assignment file.
+This is my own work that is done by myself and my team-mate only */
+
 #include <iostream>
 
 int main() {
