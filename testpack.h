0d4488e948d5dcdafedf4b7f6209c2dfd72afb36
adam <adamqinawi@gmail.com>
2021-06-21 12:15:11 +0200

    added testpack.h, unnecessary for operation but to prevent compilation errors

---
+++ testpack.h
@@ -0,0 +1,14 @@
+
+#ifndef INC_242_ASSIGNMENT_4_TESTPACK_H
+#define INC_242_ASSIGNMENT_4_TESTPACK_H
+
+#include "grid.h"
+
+int adamTest(){
+    printLegend();
+    grid board(5);
+    std::cout<<board;
+    return 0;
+};
+
+#endif //INC_242_ASSIGNMENT_4_TESTPACK_H
